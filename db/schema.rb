43031bdc536364f127cebae6b6ff22ccb4af16de
RYOTA <kobaryo04ysh@icloud.com>
2020-01-18 12:59:03 +0000

    Finish

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200116140808) do
+ActiveRecord::Schema.define(version: 20200118114320) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
